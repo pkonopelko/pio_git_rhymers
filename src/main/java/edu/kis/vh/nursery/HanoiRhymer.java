56f7ae6097c60de660444c58142e7820c37d2a4e
Piotr Robert Konopelko <206060@edu.p.lodz.pl>
2020-02-05 03:35:37 +0100

    (1.3 / topics 1-3) formatting vol. 1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,20 @@
 package edu.kis.vh.nursery;
 
+// "alt + ←" and "alt + →" helps to move between places you were, e.g. files
+// (like back / forward)
+
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
